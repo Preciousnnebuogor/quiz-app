a5c97c65efabc1d065bf978e74cc8d512ab23c5f
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2024-03-09 18:41:04 +0100

    styled the welcome folder

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -1,6 +1,7 @@
+import style from "./foot.module.scss"
 export default function () {
   return (
-    <div>
+    <div className={style.footer}>
       <h2>Privacy Policy</h2>
       <h2>Terms and Conditions</h2>
     </div>
--- app/components/heros/heros.tsx
+++ app/components/heros/heros.tsx
@@ -1,15 +1,21 @@
-import Image from 'next/image'
-export default function Heros(){
-    return <div>
-        <div>
-     <Image src="/quiz.png" height="250"
-        width="250" alt=""/>
-        </div>
-     <div>
-     <h1>QUIZ WORLD</h1>
-     <p>This app is built to encourage and enlighten you about different fields of study.
-        It has 3 categories that enables you choose your field of study.
-     </p>
-     </div>
+import Image from "next/image";
+import style from "./hero.module.scss";
+
+export default function Heros() {
+  return (
+    <div className={style.body}>
+      <div className={style.img}>
+        <Image src="/quiz.png" height="300" width="300" alt="" />
+      </div>
+      <div className={style.text}>
+        <h1 className={style.h1}>BUZ WORLD</h1>
+        <p className={style.p}>
+          This app is built to encourage and enlighten you about different
+          fields of study.<br/> It has 3 categories that enables you choose your
+          field of study. <br/> Click the button below to start your quiz.
+        </p>
+      </div>
+      <button className={style.button}>Start</button>
     </div>
+  );
 }
