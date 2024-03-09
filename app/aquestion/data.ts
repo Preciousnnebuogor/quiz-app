6a4e9a5e66576a555fbb61bba6bb6e9e58f0397f
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2024-03-09 22:48:20 +0100

    added question folder

---
+++ app/aquestion/data.ts
@@ -0,0 +1,57 @@
+interface IListQuestion {
+  question: string;
+  "option a": string;
+  "option b": string;
+  "option c": string;
+  answerindex: string;
+}
+
+const listQuestion: IListQuestion[] = [
+  {
+    question: "What is the full meaning of ECOWAS",
+    "option a": "Economic Community of West African States.",
+    "option b": "Economics Countries of West Africa Countries.",
+    "option c": "Economy Commiunties of West African Countries.",
+    answerindex: "a",
+  },
+
+  {
+    question: "Add 23 and 2",
+    "option a": "29",
+    "option b": "34",
+    "option c": "25",
+    answerindex: "c",
+  },
+
+  {
+    question: "What is the full meaning of v",
+    "option a": "jiy",
+    "option b": "vivian",
+    "option c": "cow",
+    answerindex: "b",
+  },
+
+  {
+    question: "What is the my name",
+    "option a": "precios",
+    "option b": "nnebugor",
+    "option c": "go",
+    answerindex: "b",
+  },
+
+  {
+    question: "What is the full meaning of GOAT",
+    "option a": "Greatest Of Them All",
+    "option b": "food",
+    "option c": "come",
+    answerindex: "a",
+  },
+
+  {
+    question: "What is the full meaning of ECOWAS",
+    "option a": "string",
+    "option b": "number",
+    "option c": "yes",
+    answerindex: "a",
+  },
+];
---
+++ app/aquestion/page.tsx
@@ -0,0 +1,17 @@
+export default function Question() {
+  return (
+    <div>
+      <div>
+        <p>What is the meaning of ECOWAS</p>
+        <h1>
+          a. Economic Community of West African States. <br/>
+          b. Economics Countries of West Africa Countries. <br/>
+          c. Economy Commiunties of West African Countries.
+        </h1>
+      </div>
+      <div>
+        <button>Next</button>
+      </div>
+    </div>
+  );
+}
