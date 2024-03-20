0c374e8235a076ce41f716f43629d5a24915b580
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2024-03-20 23:21:57 +0100

    working on the submit button in bresult

--- app/bresult/page.tsx
+++ app/bresult/page.tsx
@@ -6,16 +6,32 @@ import style from "./resultpage.module.scss"
 export default function ResultPage() {
   const params = useSearchParams();
   const score = params.get("score");
+
+
+//   
+
+function displayText() {
+    const text = document.getElementById("textField");
+    
+  }
+  
+
   return (
-    <div className="bg-green-200 w-[200px]">
+    <div className={style.home}>
       <p style={{
-              paddingLeft: "100px",
+              paddingLeft: "50%",
               fontWeight: "bolder",
               color: "rgb(123, 186, 29)",
+              marginTop: "5%"
             }}>{score}</p>
-            <div className={style.button}>
-      <button>Submit</button>
-      <button>Cancel</button>
+            
+            <div className={style.buttons}>
+      <button className={style.button} onCl>Submit</button>
+      <div>
+  This is the text that appears when you click the button.
+</div>
+
+      <button className={style.button}>Cancel</button>
       </div>
     </div>
   );
