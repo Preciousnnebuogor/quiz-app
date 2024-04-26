6ca51554cec9cf1f10e6092cdf631ca980ff2650
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2024-04-26 15:19:52 +0100

    first commit

--- app/components/welcome/home.tsx
+++
@@ -1,9 +0,0 @@
-import Heros from "../heros/heros";
-
-export default function Beginner() {
-  return (
-    <div>
-      <Heros />
-    </div>
-  );
-}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import Section from "./category/page";
-import Beginner from "./components/welcome/home";
+
+
 
 export default function Home() {
   return (
     <div>
-      <Beginner />
+      
     </div>
   );
 }
